20b16e2cf63a9bf3d52d4a211fc6d15c4593b7a9
Rafa <rvalverde@iesrafaelalberti.es>
2022-11-04 20:31:48 +0100

    tercero modificado

--- src/main/kotlin/Buscar.kt
+++ src/main/kotlin/Buscar.kt
@@ -10,8 +10,5 @@ object Buscar {
         println("***Buscando PJ****")
     }
 
-    fun checkPopular(personaje: String, personaje2: String) : String {
-        println("***Buscando Popularidad")
-        return ""
-    }
+
 }
--- src/main/kotlin/Comic.kt
+++
@@ -1,9 +0,0 @@
-package Ejercicio4
-
-class Comic(
-    var id: String,
-    var title: String,
-    var description: String) {
-
-
-}
